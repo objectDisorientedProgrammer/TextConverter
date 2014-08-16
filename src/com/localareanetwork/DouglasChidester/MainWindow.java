097c191caaf7646c2b1f3e03c65f02a32bd74d56
Doug Chidester <tikiman92@gmail.com>
2014-08-16 14:50:48 -0700

    Added GPLv3 header to source files.

--- src/com/localareanetwork/DouglasChidester/MainWindow.java
+++ src/com/localareanetwork/DouglasChidester/MainWindow.java
@@ -1,9 +1,21 @@
 /*
  * created Sept 10, 2013 around 11:46 PM
- * finished 
  * 
- * main gui window
- * 
+ * Main GUI for Text Converter.
+ *  Copyright (C) 2013  Douglas Chidester
+ *
+ *  This program is free software: you can redistribute it and/or modify
+ *  it under the terms of the GNU General Public License as published by
+ *  the Free Software Foundation, either version 3 of the License, or
+ *  (at your option) any later version.
+ *
+ *  This program is distributed in the hope that it will be useful,
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *  GNU General Public License for more details.
+ *
+ *  You should have received a copy of the GNU General Public License
+ *  along with this program.  If not, see <http://www.gnu.org/licenses/>.
  * To Do
  *   make window and components resize
  *   add 'tab' key
